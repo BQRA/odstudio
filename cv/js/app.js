87eaf958b14aa860ac1c2b1da17ebfb38e9e4c4c
Bora DAN <bora_dan@hotmail.com>
2021-01-10 16:58:05 +0300

    isinsezgiavci.com icin cv klasoru olusturuldu

---
+++ cv/js/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('app', []);
+
+app.controller('allControllers', function($scope, $http, $timeout) {
+
+    $http.get("data/cv-datas.json")
+    .then(function(response) {
+        $scope.cvDatas = response.data;
+        console.log(response.data);
+    }).then(function() {
+    	$scope.regexDesc()
+    });
+
+		$scope.regexDesc = function() {
+			$timeout(function(){
+				$('.section .description > span').map(function() {
+					console.log($(this).html())
+					$(this).html( marked($(this).html()) );
+				})
+			}, 0, false);
+		}
+
+})
+
+
